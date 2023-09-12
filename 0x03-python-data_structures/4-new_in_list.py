2187cbf0548954d5854932fa5dc6800454180f18
essaiydy <imaneessaiydy2@gmail.com>
2023-09-12 11:52:31 -0700

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if isinstance(my_list, list):
+        for idx in range(len(my_list) - 1):
+            my_list.copy(new_list)
+            new_list = [x for x in my_list]
+            new_list[idx] = element
+            return (new_list)
+       elif:
+           return (my_list)
