e12e0d4dc6ae0f0a416e7e7da614b7c4ec83d92a
essaiydy <imaneessaiydy2@gmail.com>
2023-09-13 15:13:34 -0700

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    new_dect = {i: j * 2 for i, j in a_dictionay}
+    new_dect = {i: j * 2 for i, j in a_dictionary.items()}
     return new_dect
