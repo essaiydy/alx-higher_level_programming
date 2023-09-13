32d3d4e41e656792866e5517206a63dd526f6825
essaiydy <imaneessaiydy2@gmail.com>
2023-09-13 13:51:27 -0700

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-   setlist = set(my_list)
-   somme = 0
-   for i in setlist:
-       somme += i
-    return(somme)
-
+    somme = 0
+    for i in set(my_list):
+        somme += i
+    return somme
