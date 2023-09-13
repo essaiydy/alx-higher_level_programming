10df6386c28ddb6e424fa255b7f129ca68e55588
essaiydy <imaneessaiydy2@gmail.com>
2023-09-13 15:47:24 -0700

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,5 +4,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     for i in a_dictionary:
-        if a_dictionary[i] == max(a_dictionary.value()):
+        if a_dictionary[i] == max(a_dictionary.values()):
             return i
