937d22868f884aa46e5119250db8f1e5b2cfd64e
essaiydy <imaneessaiydy2@gmail.com>
2023-09-13 13:10:10 -0700

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    for x in my_list:
+        if x = search:
+            new_list = list(x.replace(replace))
+    return new_list
