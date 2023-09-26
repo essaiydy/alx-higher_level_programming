0a3c295a65be61089311b01888082f81a0c5997e
essaiydy <imaneessaiydy2@gmail.com>
2023-09-26 15:15:28 -0700

    task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,6 +12,6 @@ class Square:
             raise TypeError("size must be an integer")
         '''try the seconde error'''
 
-        if size < 0 :
+        if size < 0:
             raise ValueError(size must be >= 0)
         self.__size = size
