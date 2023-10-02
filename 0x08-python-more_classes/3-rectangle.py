84379b9b8667c37afa1cf91ce373b69154ee3dbf
essaiydy <imaneessaiydy2@gmail.com>
2023-10-02 16:46:12 -0700

    task3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,4 +52,4 @@ class Rectangle:
                 for j in range(self.__width):
                     rect += '#'
                 rect += "\n"
-            return rect.strip()
+            return rect[:-1]
