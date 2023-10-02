d90910802e8bb61f881390fa5e96c62cf461ed5e
essaiydy <imaneessaiydy2@gmail.com>
2023-10-02 13:55:10 -0700

    task1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""rectangle representation"""
+
+
+class Rectangle:
+    """this is the class"""
+    def __init__(self, width = 0, height = 0):
+        self._width = width
+        self._height = height
+    @property
+    def width(self):
+        return self._width
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            self._width = value
+        else:
+            raise TypeError("width must be an integer")
+        if value < 0 :
+            raise ValueError("width must be >= 0")
+    @property
+    def height(self):
+        return self._height
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            self._height = value
+        else:
+            raise TypeError("height must be an integer")
+        if value < 0 :
+            raise ValueError("height must be >= 0")
