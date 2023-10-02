311fbc032255a432caa8f52531e76e121a2c0c6c
essaiydy <imaneessaiydy2@gmail.com>
2023-10-02 16:49:41 -0700

    task3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -45,7 +45,7 @@ class Rectangle:
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
-            print("")
+            return("")
         else:
             rect = ""
             for i in range(self.__height):
