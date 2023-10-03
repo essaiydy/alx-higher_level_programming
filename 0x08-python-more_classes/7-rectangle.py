de6a54431100db32688484f527bf1fa27e4e9e50
essaiydy <imaneessaiydy2@gmail.com>
2023-10-03 06:00:02 -0700

    task7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -54,7 +54,7 @@ class Rectangle:
             rect = ""
             for i in range(self.__height):
                 for j in range(self.__width):
-                    rect += Rectangle.print_symbol
+                    rect += str(self.print_symbol)
                 rect += "\n"
             return rect[:-1]
 
