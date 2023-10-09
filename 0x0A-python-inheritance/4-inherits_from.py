b96e82a11ece7926ea92e1b0cc28a00aa3bb66d5
essaiydy <imaneessaiydy2@gmail.com>
2023-10-09 15:44:31 -0700

    t0

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,6 @@
 def inherits_from(obj, a_class):
     '''traitement'''
 
-    return issubclass(type(obj), a_class)
+    if isinstance(obj, a_class) and not type(obj) is a_class:
+        return True
+    return False
