4b3d93650f9cfe08ecc7963e4072082acbb19eb7
essaiydy <imaneessaiydy2@gmail.com>
2023-11-14 15:09:57 -0800

    first sql exercice

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
---Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- this command show u the commande that creat this tabel
 SHOW CREATE TABLE first_table;
