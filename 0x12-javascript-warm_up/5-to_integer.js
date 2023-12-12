ea69e52728a63082270f1c6c0e53fab34efc613f
essaiydy <imaneessaiydy2@gmail.com>
2023-12-11 16:05:34 -0800

    tasks of js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-console.log(`My number: ${parseInt(process.argv[2])}`);
+if (parseInt(process.argv[2])) {
+  console.log(`My number: ${parseInt(process.argv[2])}`);
+}
+
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+}
