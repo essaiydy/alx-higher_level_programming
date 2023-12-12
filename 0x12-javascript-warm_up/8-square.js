33ba455403b7595842ab31ec8994ed0d0d5b88f4
essaiydy <imaneessaiydy2@gmail.com>
2023-12-11 16:46:46 -0800

    tasks of js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-
-const size = parseInt(process.argv[2]);
-
-if (Number.isInteger(size)) {
-  for (let i = 0; i < size; i++) {
-    let row = '';
-    for (let j = 0; j < size; j++) {
-      row += 'x';
-    }
-    console.log(row);
-  }
-} else {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
 }
