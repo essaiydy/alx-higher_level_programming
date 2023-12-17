5aecad228e363c0199bf077b9f2cf785530b8820
essaiydy <imaneessaiydy2@gmail.com>
2023-12-17 06:34:37 -0800

    task

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-'''Write a script that takes in the name of a state as an argument and lists all cities of that state'''
+'''Write a script that takes in the name of a state as an
+argument and lists all cities of that state'''
 
 if __name__ == '__main__':
 
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states ON states.id = cities.state_id\
-            WHERE states.name = %s ORDER BY cities.id", (sys.argv[4],))
+    cur.execute("SELECT cities.name FROM cities JOIN states \
+            ON states.id = cities.state_id \
+            WHERE states.name = %s", (sys.argv[4],))
 
     rows = cur.fetchall()
 
@@ -19,4 +26,3 @@ if __name__ == '__main__':
 
     cur.close()
     db.close()
-
