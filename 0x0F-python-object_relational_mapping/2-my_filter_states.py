46f3a2fc4aa39ff06ade9f2bcc697ec9cd24b6be
essaiydy <imaneessaiydy2@gmail.com>
2023-12-17 06:13:59 -0800

    task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""script that takes in an argument and displays all values in the states table"""
+"""script that takes in an argument and
+displays all values in the states table"""
+
 
 if __name__ == '__main__':
 
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], password=sys.argv[2],
-        db=sys.argv[3])
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute("select * from states where (name = %s) order by states.id ASC", (sys.argv[4],))
+    cursor.execute("select * from states where name = '{}'".format(sys.argv[4]))
 
     rows = cursor.fetchall()
 
