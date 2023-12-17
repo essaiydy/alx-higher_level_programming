b5c0d0efccdced01d400a0dd0a9c3af0ebe39c61
essaiydy <imaneessaiydy2@gmail.com>
2023-12-17 05:47:18 -0800

    task

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Write a script that takes in the name of a state as an argument and lists all cities of that state'''
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities JOIN states ON states.id = cities.state_id\
+            WHERE states.name = %s ORDER BY cities.id", (sys.argv[4],))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Write a python file that contains the class definition of a State'''
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+
+     __tablename__ = 'states'
+
+     id = Column(Integer, primary_key=True)
+     name =  Column(String(128), nullable=False)
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format("root", "root", "hbtn_0e_4_usa"), pool_pre_ping=True)
+
+Base.metadata.create_all(engine)
