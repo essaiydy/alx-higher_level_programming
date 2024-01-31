32bec4fec71066f9c50c99a0914047dd2c518704
essaiydy <imaneessaiydy2@gmail.com>
2024-01-30 17:16:13 -0800

    tak 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 //script that reads and prints the content of a file.
-
 const fs = require('fs');
-
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
